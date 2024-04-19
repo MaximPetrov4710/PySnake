c69f09fddba344f8e831f968e4d2d6f7b43ed140
Max <maxpetrov4710@yandex.ru>
2024-04-19 08:20:56 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Hi this is my first project you don't need to download src
---
+++ SnakeGame.py
@@ -0,0 +1,91 @@
+import pygame
+import sys
+
+
+SCREEN_SIZE = (800, 600)
+
+SNAKE_ICON = "1670848189_grizly-club-p-pikseli-png-21.jpg"
+
+GAME_TITLE = "Snake"
+
+GAME_SPEED = 10
+
+BACKGROUND_COLOR = (0, 0, 255)
+
+
+def initialize_pygame():
+    pygame.init()
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    icon = pygame.image.load(SNAKE_ICON)
+    pygame.display.set_icon(icon)
+    pygame.display.set_caption("Snake")
+    clock = pygame.time.Clock()
+    return screen, clock
+
+
+def initialize_game_state():
+    game_state = {
+        "program_running": True,
+        "game_running": False
+    }
+    return game_state
+
+
+def perform_shutdown():
+    pygame.quit()
+    sys.exit()
+
+def get_events():
+    events = []
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            events.append("quit")
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                events.append("up")
+            elif event.key == pygame.K_DOWN:
+                events.append("down")
+            elif event.key == pygame.K_LEFT:
+                events.append("left")
+            elif event.key == pygame.K_RETURN:
+                events.append("enter")
+            elif event.key == pygame.K_SPACE:
+                events.append("space")
+            elif event.key == pygame.K_ESCAPE:
+                events.append("escape")
+            elif event.key == pygame.K_RIGHT:
+                events.append("right")
+    return events
+
+def update_game_state(game_state):
+    pass
+
+def update_screen(screen, game_state):
+    screen.fill((BACKGROUND_COLOR))
+    pygame.display.flip()
+
+
+def main():
+    screen, clock = initialize_pygame()
+    game_state = initialize_game_state()
+    while game_state["program_running"]:
+        clock.tick(GAME_SPEED)
+        events = get_events()
+        print(events)
+        update_game_state(game_state)
+        update_screen(screen, game_state)
+    perform_shutdown()
+
+main()
+
+# pygame.init()
+# xscreen = pygame.display.set_mode((640, 480))
+#
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             sys.exit()
+#
+#         xscreen.fill((0, 0, 0))
+#         pygame.display.flip()
